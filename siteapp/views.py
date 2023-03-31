f75a8e0db1ab4cf94d2a2d35d6fb378148bcf7f0
TAKTAK000002 <takuya.otsu@tokai-soft.co.jp>
2023-03-31 13:38:05 +0900

    MultipleSelect_Sample

--- siteapp/views.py
+++ siteapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post,ERRORCASE
 from .forms import PostForm,ErrorForm
-from django.shortcuts import redirect
+from django.shortcuts import redirect,render
 # Create your views here.
 
 
